bdc33c1aeebeadb02880023405a3917bb074ecea
Huong Bui <huongbq@vmogroup.com>
2025-03-31 17:37:14 +0700

    second commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 for (let i = 0; i < 10; i++) {
   i = i++;
 }
-console.log("i=", i);
+// console.log("i=", i);
 
 // for/in: loops through the properties of an object
 const object = { a: 1, b: 2, c: 3 };
@@ -42,13 +42,20 @@ function greet(name) {
   console.log(`Hello, ${name}!`);
 }
 
+greet("huong");
+
 //function declaration
 function add(a, b) {
   return a + b;
 }
+add(1, 2);
 
 // function arrow
-const multiply = (a, b) => a * b;
+const multiply = (a, b) => {
+  a * b;
+  console.log("sum", a * b);
+};
+multiply(3, 4);
 
 // Objects & Arrays (key-value pairs, array methods like map, filter, reduce)
 const person = {
@@ -62,3 +69,58 @@ const filterNumber = numbers.filter((x) => x > 3);
 const reduceNumber = numbers.reduce(
   (accumulator, current) => accumulator + current
 );
+
+// Destructuring Arrays
+const names = ["Alice", "Bob", "Charlie", "Dave"];
+const [name1, name2, name3, name4] = names;
+
+console.log(name1);
+
+// Destructuring Objects
+const personn = {
+  name: "Alice",
+  age: 25,
+  address: {
+    city: "New York",
+    state: "NY",
+  },
+};
+
+const { namee, age, address } = personn;
+
+console.log(namee);
+console.log(age);
+console.log(address);
+
+// Destructuring Nested Objects
+const personnn = {
+  nameee: "Alice",
+  age: 25,
+  address: {
+    city: "New York",
+    state: "NY",
+  },
+};
+
+const {
+  nameee,
+  agee,
+  address: { city, state },
+} = personnn;
+
+console.log(nameee);
+console.log(agee);
+console.log(city);
+console.log(state);
+
+// Rest Operator
+function sum(...numbers) {
+  return numbers.reduce((total, number) => total + number, 0);
+}
+
+console.log(sum(1, 2, 3, 4, 5));
+
+// Spread Operator
+const numberss = [1, 2, 3, 4, 5];
+
+console.log(...numberss);
